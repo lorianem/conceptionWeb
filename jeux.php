58f1aa6d6ca3b88e9a1ddafa17e7f9df3ac89867
bontedjp <bonte.djulu@groupe-esigelec.org>
2023-11-17 09:48:53 +0100

    essai push

--- jeux.php
+++ jeux.php
@@ -1,11 +1,14 @@
 <?php include("code/EnTete.php") ?>
 <?php include("code/relocalisationVisiteur.php")?>
 
+<!doctype html>
 <html lang="fr">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Jeux</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="???.css">
   </head>
 
   <body>
@@ -17,7 +20,7 @@
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="home.php.php">Accueil</a>
+          <a class="nav-link active" aria-current="page" href="home.php">Accueil</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="jeux.php">Jeux</a>
@@ -25,24 +28,21 @@
         <li class="nav-item">
           <a class="nav-link" href="planning.php">Planning</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="planning.php">Planning</a>
-        </li>
-        <!-- Si role est existe dans la session, l'utilisateur est connecté  -->
               </ul>
       <ul class="navbar-nav mb-lg-0">
-        <!-- Utilisateur non connecté (on affiche Inscription et Connection) -->
-                <li class="nav-item">
-          <a class="nav-link"  href="inscription.php">Inscription</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="connexion.php">Connexion</a>
-        </li>
-        <!-- Sinon on affiche la Déconnexion -->
-        
 
+        <li class="nav-item">
+          <img src="editer-le-profil.png" class="rounded mx-auto d-block" alt="Mon Profil">
+          <a class="nav-link"  href="profil.php">Profil</a>
+        </li>
       </ul>
 
     </div>
   </div>
 </nav>
+
+
+
+
+  </body>
+<html>
