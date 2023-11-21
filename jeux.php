9d10212a9286820cb3d3c62890d537f6bca174a4
bontedjp <bonte.djulu@groupe-esigelec.org>
2023-11-21 16:19:58 +0100

    Avancement code planning, jeux et homePage

--- jeux.php
+++ jeux.php
@@ -8,11 +8,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Jeux</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="???.css">
+    <link rel="stylesheet" href="style.css">
   </head>
 
   <body>
-  
+  <nav class="navbar navbar-expand-md bg-dark border-bottom border-body" data-bs-theme="dark">
+  <div class="container-fluid">
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="home.php">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="jeux.php">Jeux</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="planning.php">Planning</a>
+        </li>
+              </ul>
+      <ul class="navbar-nav mb-lg-0">
+
+        <li class="nav-item">
+          <img src="editer-le-profil.png" class="rounded mx-auto d-block" alt="Mon Profil">
+          <a class="nav-link"  href="profil.php">Profil</a>
+        </li>
+      </ul>
+
+    </div>
+  </div>
+</nav>
 
 <main>
 
