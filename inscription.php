525b16d83c2bb060c602417504ba2ac891ac0f74
loriane_mech <loriane.mechiche@gmail.com>
2023-11-24 12:28:58 +0100

    Mise en forme connection

--- inscription.php
+++ inscription.php
@@ -121,86 +121,56 @@ else
 
    <script src="https://www.google.com/recaptcha/api.js"></script>
 	<main>
-		<div align="center"  class="page">
+		<section class="page">
          <h2>Inscription</h2>
          <br />
          <form method="POST" action="" id="inscription">
-            <table>
-               <p id='erreurmess'></p> 
             	
-            	<tr>
-                  <td align="right">
-                     <label for="nom">Nom :</label>
+            	<div class="mb-3"> 
+                     <label class="form-label" for="nom">Nom :</label>
+                     <input class="form-control" type="text" placeholder="Votre Nom" id="nom" name="nom" value="<?php if(isset($nom)) { echo $nom; } ?>" />
                   </td>
-                  <td>
-                     <input type="text" placeholder="Votre Nom" id="nom" name="nom" value="<?php if(isset($nom)) { echo $nom; } ?>" />
-                  </td>
-               </tr>
-               <tr>
-                  <td align="right">
-                     <label for="prenom">Prenom :</label>
-                  </td>
-                  <td>
-                     <input type="text" placeholder="Votre prenom" id="prenom" name="prenom" value="<?php if(isset($prenom)) { echo $prenom; } ?>" />
-                  </td>
-               </tr>
-               <tr>
-                  <td align="right">
-                     <label for="pseudo">Pseudo :</label>
-                  </td>
-                  <td>
-                     <input type="text" placeholder="Votre pseudo" id="pseudo" name="pseudo" value="<?php if(isset($pseudo)) { echo $pseudo; } ?>" />
-                  </td>
-               </tr>
-               <tr>
-                  <td align="right">
-                     <label for="mail">Mail :</label>
-                  </td>
-                  <td>
-                     <input type="email" placeholder="Votre mail" id="mail" name="mail" value="<?php if(isset($mail)) { echo $mail; } ?>" />
-                  </td>
-               </tr>
-               <tr>
-                  <td align="right">
-                     <label for="mdp">Mot de passe :</label>
-                  </td>
-                  <td>
-                     <input type="password" placeholder="Votre mot de passe" id="mdp1" name="mdp1" />
-                  </td>
-               </tr>
-               <tr>
-                  <td align="right">
-                     <label for="mdp2">Confirmation du mot de passe :</label>
-                  </td>
-                  <td>
-                     <input type="password" placeholder="Confirmez votre mdp" id="mdp2" name="mdp2" />
-                  </td>
-               </tr>
-               <tr>
-                  <td align="right"><label>Recapcha : </label></td>
-                  <td>
+               </div>
+               <div class="mb-3"> 
+
+                     <label class="form-label" for="prenom">Prenom :</label>
+                     <input class="form-control" type="text" placeholder="Votre prenom" id="prenom" name="prenom" value="<?php if(isset($prenom)) { echo $prenom; } ?>" />
+               </div>
+               <div class="mb-3"> 
+                     <label class="form-label" for="pseudo">Pseudo :</label>
+                     <input class="form-control" type="text" placeholder="Votre pseudo" id="pseudo" name="pseudo" value="<?php if(isset($pseudo)) { echo $pseudo; } ?>" />
+               </div>
+               <div class="mb-3"> 
+                     <label class="form-label" for="mail">Mail :</label>
+               
+                     <input class="form-control" type="email" placeholder="Votre mail" id="mail" name="mail" value="<?php if(isset($mail)) { echo $mail; } ?>" />
+                  
+               </div>
+               <div class="mb-3"> 
+                     <label class="form-label" for="mdp">Mot de passe :</label>
+                     <input class="form-control" type="password" placeholder="Votre mot de passe" id="mdp1" name="mdp1" />
+               </div>
+               <div class="mb-3"> 
+                     <label class="form-label" for="mdp2">Confirmation du mot de passe :</label>
+
+                     <input class="form-control" type="password" placeholder="Confirmez votre mdp" id="mdp2" name="mdp2" />
+               </div>
+               <div class="mb-3"> 
+                  
                      <form method="POST">
                         <div class="g-recaptcha" data-sitekey="6LfSLdEUAAAAADYFitII6rlfJn6ePMHnz4KGPFie"></div>
                      </form>
-                  </td>
-               </tr>
-               <tr>
-                  <td align="right"><br> <a  href="connexion.php"> Se Connecter </a>  </td>   
-                     
-                  <td align="center">
-                     <br />
-                     <input type="submit" name="envoyer" value="Je m'inscris" />
-                  </td>
-               </tr>
-               
-            </table>
-         </form><br>
-         <?php
-         echo '<font color="red">'.$msg."</font>";
+               </div>
+               <div class="mb-3"> 
+                   
+                     <?php echo '<font color="red">'.$msg."</font>"; ?>
+                  <input class="btn btn-primary" type="submit" name="envoyer" value="Je m'inscris" /><br><br>
+                  <a  href="connection.php"> Se Connecter </a> 
+               </div>
+         </form>
          
-         ?>
    
-      </div>
+      </section>
 
 	</main>
 
